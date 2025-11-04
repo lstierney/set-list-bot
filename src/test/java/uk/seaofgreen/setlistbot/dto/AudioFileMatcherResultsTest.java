1c14bd0691de9d9ab6be4a83c4a26bf37530c089
Lawrence Tierney <lawrence.tierney@protonmail.com>
2025-11-04 14:30:40 +0000

    SLB-5 If some Songs cannot be matched to an audio file then don't return Playlist, return JSON of failed matches.

---
+++ src/test/java/uk/seaofgreen/setlistbot/dto/AudioFileMatcherResultsTest.java
@@ -0,0 +1,54 @@
+package uk.seaofgreen.setlistbot.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.seaofgreen.setlistbot.model.Song;
+import uk.seaofgreen.setlistbot.utils.TestUtils;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AudioFileMatcherResultsTest {
+    private AudioFileMatcherResults audioFileMatcherResults;
+
+    @BeforeEach
+    void setup() {
+        audioFileMatcherResults = new AudioFileMatcherResults();
+    }
+
+    @Test
+    public void testMatches() {
+        Song song = TestUtils.getSong1();
+        Path path = Path.of("/some/path");
+
+        audioFileMatcherResults.addMatch(song, path);
+
+        Map<Song, Path> matches = audioFileMatcherResults.getMatches();
+        List<Song> notMatched = audioFileMatcherResults.getNotMatched();
+
+        assertThat(matches.size(), is(1));
+        assertThat(notMatched.size(), is(0));
+        assertTrue(matches.containsKey(song));
+        assertTrue(matches.containsValue(path));
+
+    }
+
+    @Test
+    public void testNotMatched() {
+        Song song = TestUtils.getSong1();
+
+        audioFileMatcherResults.addNotMatched(song);
+
+        Map<Song, Path> matches = audioFileMatcherResults.getMatches();
+        List<Song> notMatched = audioFileMatcherResults.getNotMatched();
+
+        assertThat(matches.size(), is(0));
+        assertThat(notMatched.size(), is(1));
+        assertTrue(notMatched.contains(song));
+    }
+}
